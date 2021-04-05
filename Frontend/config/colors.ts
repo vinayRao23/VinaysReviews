0f93d8d1e991f11e99224d866615abf34e7cdb10
CoolKid <vinay23.rao@gmail.com>
2021-04-05 09:34:44 -0700

    Added new colors to color pallet.

--- Frontend/config/colors.ts
+++ Frontend/config/colors.ts
@@ -1,10 +1,10 @@
 export default {
   medium: "#7c7c82",
-  light: "#ededf2",
   lightBlue: "#5ab5da",
   lightGreen: "#58cd7d",
   red: "red",
   white: "#fff",
   hyperlinkBlue: "#4385f4",
-  lightBlack: "#242424",
+  black: "black",
+  lightGray: "#ebeef0",
 };
