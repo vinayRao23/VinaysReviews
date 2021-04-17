36c61ab760c9a02263624e806a75d1da93fc9e5b
CoolKid <vinay23.rao@gmail.com>
2021-04-17 10:43:19 -0700

    Got rid of sync for user table.

--- Backend/Server/src/resolvers.ts
+++ Backend/Server/src/resolvers.ts
@@ -14,7 +14,6 @@ const resolvers: IResolvers = {
   },
   Mutation: {
     registerUser: async (_: void, args: UserArgsInt) => {
-      await User.sync({ force: true });
       const salt = await bcrypt.genSalt(10);
       const password = await bcrypt.hash(args.password, salt);
       const user = User.build({
